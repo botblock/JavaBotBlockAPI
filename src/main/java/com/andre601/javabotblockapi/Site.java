d0f5a9b8fad3327315f9ea135c119447533b1e05
Andre601 <11576465+Andre601@users.noreply.github.com>
2019-11-17 21:33:28 +0100

    Minor visual changes.

--- src/main/java/com/andre601/javabotblockapi/Site.java
+++ src/main/java/com/andre601/javabotblockapi/Site.java
@@ -31,22 +31,27 @@ public enum Site {
      * <a href="https://botlist.space" target="_blank">botlist.space</a>
      */
     BOTLIST_SPACE("botlist.space"),
+    
     /**
      * <a href="https://botsfordiscord.com" target="_blank">botsfordiscord.com</a>
      */
     BOTSFORDISCORD_COM("botsfordiscord.com"),
+    
     /**
      * <a href="https://bots.ondiscord.xyz" target="_blank">bots.ondiscord.xyz</a>
      */
     BOTS_ONDISCORD_XYZ("bots.ondiscord.xyz"),
+    
     /**
      * <a href="https://discordapps.dev" target="_blank">discordapps.dev</a>
      */
     DISCORDAPPS_DEV("discordapps.dev"),
+    
     /**
      * <a href="https://discord.boats" target="_blank">discord.boats</a>
      */
     DISCORD_BOATS("discord.boats"),
+    
     /**
      * <a href="https://discordbots.org" target="_blank">discordbots.org</a>
      * 
@@ -55,48 +60,59 @@ public enum Site {
      */
     @Deprecated
     DISCORDBOTS_ORG("discordbots.org"),
+    
     /**
      * <a href="https://discordbotlist.com" target="_blank">discordbotlist.com</a>
      */
     DISCORDBOTLIST_COM("discordbotlist.com"),
+    
     /**
      * <a href="https://discordbotreviews.xyz" target="_blank">discordbotreviews.xyz</a>
      */
     DISCORDBOTREVIEWS_XYZ("discordbotreviews.xyz"),
+    
     /**
      * <a href="https://discordbot.world" target="_blank">discordbot.world</a>
      */
     DISCORDBOT_WORLD("discordbot.world"),
+    
     /**
      * <a href="https://discord.bots.gg" target="_blank">discord.bots.gg</a>
      */
     DISCORD_BOTS_GG("discord.bots.gg"),
+    
     /**
      * <a href="https://discordextremelist.xyz" target="_blank">discordextremelist.xyz</a>
      * 
      * @since v2.3.3
      */
     DISCORDEXTREMELIST_XYZ("discordextremelist.xyz"),
+    
     /**
      * <a href="https://discordsbestbots.xyz" target="_blank">discordsbestbots.xyz</a>
      */
     DISCORDSBESTBOTS_XYZ("discordsbestbots.xyz"),
+    
     /**
      * <a href="https://discordbots.fun" target="_blank">discordbots.fun</a>
      */
     DISCORDBOTS_FUN("discordbots.fun"),
+    
     /**
      * <a href="https://divinediscordbots.com" target="_blank">divinediscordbots.com</a>
      */
     DIVINEDISCORDBOTS_COM("divinediscordbots.com"),
+    
     /**
      * <a href="https://lbots.org" target="_blank">lbots.org</a>
      */
     LBOTS_ORG("lbots.org"),
+    
     /**
      * <a href="https://mythicalbots.xyz" target="_blank">mythicalbots.xyz</a>
      */
     MYTHICALBOTS_XYZ("mythicalbots.xyz"),
+    
     /**
      * <a href="https://top.gg" target="_blank">top.gg</a>
      * <br>Note that top.gg is discordbots.org (new domain).
@@ -104,10 +120,12 @@ public enum Site {
      * @since v3.0.0
      */
     TOP_GG("top.gg"),
+    
     /**
      * <a href="https://wonderbotlist.com" target="_blank">wonderbotlist.com</a>
      */
     WONDERBOTLIST_COM("wonderbotlist.com"),
+    
     /**
      * <a href="https://yabl.xyz" target="_blank">yabl.xyz</a>
      * 
@@ -122,7 +140,7 @@ public enum Site {
     }
 
     /**
-     * Gets the site that was selected.
+     * Gives the ID of the selected site, which is used as ID in the BotBlock.org API.
      *
      * @return The selected site.
      */
