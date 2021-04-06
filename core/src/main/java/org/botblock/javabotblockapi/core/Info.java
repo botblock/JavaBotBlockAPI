9df097af8d1c67e5a37aa1014a3420aeb885ab84
Andre601 <11576465+Andre601@users.noreply.github.com>
2021-04-06 18:50:59 +0200

    Improve Javadoc cpmment and bump version

--- core/src/main/java/org/botblock/javabotblockapi/core/Info.java
+++ core/src/main/java/org/botblock/javabotblockapi/core/Info.java
@@ -39,7 +39,7 @@ public class Info{
     /**
      * Patch version of the Wrapper.
      */
-    public static final int PATCH = 4;
+    public static final int PATCH = 5;
     
     /**
      * Full version in the format {@code major.minor.patch}.
